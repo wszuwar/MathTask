5298ad21c1e6dbab806455a59c8dc3fd52d93533
“wszuwar <msuwr315@gmail.com>
2018-09-23 21:33:11 +0200

    Security

--- src/main/java/com/maths/mathTasks/controller/MathTaskController.java
+++ src/main/java/com/maths/mathTasks/controller/MathTaskController.java
@@ -17,37 +17,36 @@ import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 public class MathTaskController {
 
 
-
     @Autowired
     private DbService service;
 
     @Autowired
     private MathTaskMapper mathTaskMapper;
 
-
-
     @RequestMapping(method = RequestMethod.GET, value = "getMathTasks")
-    public List<MathTaskDto> getMathTasks(){
+    public List<MathTaskDto> getMathTasks() {
 
-       return mathTaskMapper.mapToMathTaskDtoList(service.getAllMathTasks());
+        return mathTaskMapper.mapToMathTaskDtoList(service.getAllMathTasks());
 
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getMathTask")
-    public MathTaskDto getMathTask(@RequestParam Long mathTaskId) throws MathTaskNotFound{
+    public MathTaskDto getMathTask(@RequestParam Long mathTaskId) throws MathTaskNotFound {
         return mathTaskMapper.mapToMathTaskDto(service.getMathTask(mathTaskId).orElseThrow(MathTaskNotFound::new));
     }
+
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteMathTask")
-    public void deleteMathTask(@RequestParam Long mathTaskId){
-            service.deleteMathTask(mathTaskId);
+    public void deleteMathTask(@RequestParam Long mathTaskId) {
+        service.deleteMathTask(mathTaskId);
     }
+
     @RequestMapping(method = RequestMethod.PUT, value = "updateMathTask")
-    public MathTaskDto updateMathTask(@RequestBody MathTaskDto mathTaskDto){
+    public MathTaskDto updateMathTask(@RequestBody MathTaskDto mathTaskDto) {
         return mathTaskMapper.mapToMathTaskDto(service.saveMathTask(mathTaskMapper.mapToMathTask(mathTaskDto)));
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createMathTask", consumes = APPLICATION_JSON_VALUE)
-    public void createMathTask(@RequestBody MathTaskDto mathTaskDto){
+    public void createMathTask(@RequestBody MathTaskDto mathTaskDto) {
         service.saveMathTask(mathTaskMapper.mapToMathTask(mathTaskDto));
     }
 
--- src/main/java/com/maths/mathTasks/spring/security/SpringSecurityConfig.java
+++ src/main/java/com/maths/mathTasks/spring/security/SpringSecurityConfig.java
@@ -2,31 +2,45 @@ package com.maths.mathTasks.spring.security;
 
 import com.maths.mathTasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     AdminConfig adminConfig;
 
-
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser(adminConfig.getAdminName())
-                .password(adminConfig.getAdminPassword())
-                .roles(adminConfig.getAdminRole());
-
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests().anyRequest().hasRole("ADMIN").and().httpBasic();
-
+        http.authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService(){
+        UserDetails admin =
+                User.withDefaultPasswordEncoder()
+                .username(adminConfig.getAdminName())
+                .password(adminConfig.getAdminPassword())
+                .roles(adminConfig.getAdminRole())
+                .build();
+        return new InMemoryUserDetailsManager(admin);
     }
 }
